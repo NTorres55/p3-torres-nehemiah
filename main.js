a9f56dfe1d2ef759f8ad30ea3a8247ad1ae0cd39
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-04-18 01:41:41 -0400

    Add new layer in html

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const myBoxes = document.getElementsByClassName('square');
+const mySecondBoxes = document.getElementsByClassName('square-2');
 
 function loopThrough() {
 
-  const changeColor = () => this.classList.toggle('red');
+  const changeColor = () => this.classList.toggle('blink');
   changeColor();
 
-  for (let i = 0; i < this.textContent.length; i++) {
-    changeColor();
+  for (let i = 0; i < this.textContent.length * 2; i++) {
+    setTimeout(changeColor, i * 1000);
   }
 };
 
+function changeLayer() {
+
+  const swap = () => {
+
+  }
+
+  setTimeout(swap, );
+};
+
+// Event Listener
 for (let i = 0; i < myBoxes.length; i++) {
   myBoxes[i].addEventListener('click', loopThrough);
+  mySecondBoxes[i].addEventListener('click', loopThrough);
 };
