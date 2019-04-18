4443c598c32035b0a5aa2b6e726a07ceb8593203
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-04-18 02:31:20 -0400

    Add second layer functionality

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const myBoxes = document.getElementsByClassName('square');
 const mySecondBoxes = document.getElementsByClassName('square-2');
 
+const layerOne = document.getElementbyId('layer-one');
+const layerTwo = document.getElementbyId('layer-two');
+
+//WK 11 101.24//
+// Functionality
 function loopThrough() {
-
   const changeColor = () => this.classList.toggle('blink');
-  changeColor();
-
   for (let i = 0; i < this.textContent.length * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
 };
 
 function changeLayer() {
-
   const swap = () => {
-
-  }
-
-  setTimeout(swap, );
+    layerOne.classList.add('hide');
+    layerTwo.classList.remove('hide');
+  };
+  setTimeout(swap, this.textContent.length * 2000);
 };
 
 // Event Listener
 for (let i = 0; i < myBoxes.length; i++) {
   myBoxes[i].addEventListener('click', loopThrough);
+  myBoxes[i].addEventListener('click', changeLayer);
   mySecondBoxes[i].addEventListener('click', loopThrough);
 };
