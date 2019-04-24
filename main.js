4302e8d5412188052614c46978199ac0efa04e8e
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-04-24 02:04:36 -0400

    Add third layer functionality and adjust CSS to make visible

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const myBoxes = document.getElementsByClassName('square');
 const mySecondBoxes = document.getElementsByClassName('square-2');
+const myThirdBoxes = document.getElementsByClassName('square-3');
 
 const layerOne = document.getElementById('layer-one');
 const layerTwo = document.getElementById('layer-two');
+const layerThree = document.getElementById('layer-three');
 
 // Functionality
 function loopThrough() {
   const changeColor = () => this.classList.toggle('blink');
-  for (let i = 0; i < this.textContent.length * 2; i++) {
+  for ( let i = 0; i < this.textContent.length * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
 };
@@ -20,9 +22,18 @@ function changeLayer() {
   setTimeout(swap, this.textContent.length * 2000);
 };
 
+function changeLayerTwo() {
+  const swap = () => {
+    layerTwo.classList.add('hide');
+    layerThree.classList.remove('hide');
+  };
+  setTimeout(swap, this.textContent.length * 2000);
+};
+
 // Event Listener
 for (let i = 0; i < myBoxes.length; i++) {
   myBoxes[i].addEventListener('click', loopThrough);
   myBoxes[i].addEventListener('click', changeLayer);
   mySecondBoxes[i].addEventListener('click', loopThrough);
+  mySecondBoxes[i].addEventListener('click', changeLayerTwo);
 };
